0f0ddd244caaad439c7a8d44bb4ecb88dd2808bb
Fei <faisputra272@gmail.com>
2024-01-25 09:42:26 +0700

    Update other.jsx

--- src/component/other.jsx
+++ src/component/other.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import img1 from '../assets/UC.png'
+import art4 from '../assets/404.png'
 
 export default function Other() {
   return (
     <div>
       <section style={{textAlign:'center'}}>
-            <img style={{width:'800px'}} src={img1} alt="" />
+            <img style={{width:'500px'}} src={art4} alt="" />
         </section>
     </div>
   )
